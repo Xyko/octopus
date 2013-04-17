e9c5857cb4cd3d5a3fa4c535f9d4f7a5abc050ee
Xyko <francisco@corp.globo.com>
2013-04-17 06:23:54 -0300

    Commit iniicial.

---
+++ tribal.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+cd /opt/prjs/XykoRuby/tribal 
+wget http://br48.tribalwars.com.br/map/player.txt -O /tmp/playerbr48.txt
+wget http://br48.tribalwars.com.br/map/village.txt -O /tmp/villagebr48.txt
+wget http://br48.tribalwars.com.br/map/ally.txt -O /tmp/allybr48.txt
+if [ -f playerbr48.txt ]
+then
+ rm playerbr48.txt
+fi
+if [ -f villagebr48.txt ]
+then
+ rm villagebr48.txt
+fi
+if [ -f allybr48.txt ]
+then
+ rm allybr48.txt
+fi
+mv /tmp/playerbr48.txt ~/Dropbox/CRuby/Tribal/lib/ 
+mv /tmp/villagebr48.txt ~/Dropbox/CRuby/Tribal/lib/
+mv /tmp/allybr48.txt ~/Dropbox/CRuby/Tribal/lib/ 
+
