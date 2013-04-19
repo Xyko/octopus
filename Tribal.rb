0770a995e13a10fd929ac61840d50b61166a7ef1
Xyko <francisco@corp.globo.com>
2013-04-19 06:09:26 -0300

    xykoBR

--- Tribal.rb
+++ Tribal.rb
@@ -445,9 +445,7 @@ class Tribal
 	def attackSpy
 		cont = 0
 
-		ville = getVillage(master?)
-		puts ville.inspect
-		exit(0)
+		ville = getVillage("xykoBR")
 
 			@temp_vector = janelaSpy(ville,9).sort_by {|_key, value| value}
 			cont = 0
