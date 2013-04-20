e5120a6b6711c58169b32765cddca183a8df3642
Xyko <francisco@corp.globo.com>
2013-04-20 07:29:49 -0300

    auto.

--- automatic.rb
+++ automatic.rb
@@ -6,7 +6,7 @@ while tst
 	slp = 60 * (30 + Random.rand(30))
 	system("date >> lixo.txt")#;ruby Tribal.rb -c farm")
 	break if $?.exitstatus != 0
-    system("date aguarde #{slp/60} minutos.. >> lixo.txt")
+    system("cat 'aguarde #{slp/60} minutos..' >> lixo.txt")
 	sleep(20)
 
 end
