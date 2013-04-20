5b72dfb24186393d2515753044526dc66da22e5a
Xyko <francisco@corp.globo.com>
2013-04-20 07:55:26 -0300

    auto.

--- automatic.rb
+++ automatic.rb
@@ -6,7 +6,7 @@ while tst
 	slp = 60 * (30 + Random.rand(30))
 	f = File.open(File.expand_path(File.dirname(__FILE__) ).to_s + "controle","w+")
 	time1 = Time.now
-	agora = "Current Time : " + time1.day +  "/" +time1.month + " "+ time1.hour + ":" + time1.min + ":" + time1.sec
+	agora = "Current Time : " + time1.day.to_s +  "/" +time1.month.to_s + " "+ time1.hour.to_s + ":" + time1.min.to_s + ":" + time1.sec.to_s
 	f.puts agora 
 	#system("ruby Tribal.rb -c farm")
 	break if $?.exitstatus != 0
