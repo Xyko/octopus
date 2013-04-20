0a5e93178fdc1d7092fed3644208ba6863c50eff
Xyko <francisco@corp.globo.com>
2013-04-20 07:49:09 -0300

    auto.

--- automatic.rb
+++ automatic.rb
@@ -4,9 +4,10 @@ tst = true
 while tst
 
 	slp = 60 * (30 + Random.rand(30))
-	system("date >> lixo.txt")#;ruby Tribal.rb -c farm")
+	f = File.open(File.expand_path(File.dirname(__FILE__) ).to_s + "controle","w+")
+	f.puts DateTime.now 
+	#system("ruby Tribal.rb -c farm")
 	break if $?.exitstatus != 0
-    system("cat '#{slp/60}'  >> lixo.txt")
 	sleep(20)
 
 end
