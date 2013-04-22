3ab074489d7e1e96ff887984b52c0d817bb54311
Xyko <francisco@corp.globo.com>
2013-04-22 10:47:19 -0300

    auto.

--- automatic.rb
+++ automatic.rb
@@ -5,9 +5,18 @@ while tst
 
 	slp = 60 * (30 + Random.rand(30))
 	f = File.open(File.expand_path(File.dirname(__FILE__) ).to_s + "/controle","a")
+	slp = 60 * (30 + Random.rand(30))
 	time1 = Time.now
-	agora = "Current Time : " + time1.day.to_s +  "/" +time1.month.to_s + " "+ time1.hour.to_s + ":" + time1.min.to_s + ":" + time1.sec.to_s
-	f.puts "#{agora} waiting #{slp/60}" 
+	time2 = time1 + slp
+	timeInfo = sprintf "Current Time : %s/%s   %s:%s -> %s:%s.\n",
+	time1.day.to_s,
+	time1.month.to_s,
+	time1.hour.to_s,
+	time1.min.to_s,
+	time2.hour.to_s,
+	time2.min.to_s
+	puts timeInfo
+	f.puts timeInfo
 	system("ruby Tribal.rb -c farm")
 	break if $?.exitstatus != 0
 	f.close unless f.closed?
