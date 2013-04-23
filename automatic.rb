1a124cd9b8798345668ad6ff9c9b8b92efd0b614
Xyko <francisco@corp.globo.com>
2013-04-23 06:30:55 -0300

    nada

--- automatic.rb
+++ automatic.rb
@@ -1,25 +1,28 @@
 require 'rubygems'
+require 'logger'
+
+log  = Logger.new("a.log") 
+log.level = Logger::INFO
 
 tst = true
 while tst
 
-	slp = 60 * (30 + Random.rand(30))
-	f = File.open(File.expand_path(File.dirname(__FILE__) ).to_s + "/controle","a")
-	slp = 60 * (30 + Random.rand(30))
-	time1 = Time.now
-	time2 = time1 + slp
-	timeInfo = sprintf "Current Time : %s/%s   %s:%s -> %s:%s.\n",
+	slp = 60 * (15 + Random.rand(15))
+	log.info(sprintf "Current Time : %s/%s   %s:%s -> %s:%s.\n",
 	time1.day.to_s,
 	time1.month.to_s,
 	time1.hour.to_s,
 	time1.min.to_s,
 	time2.hour.to_s,
-	time2.min.to_s
-	puts timeInfo
-	f.puts timeInfo
+	time2.min.to_s)
+	time1 = Time.now
+	time2 = time1 + slp
+	time2 = time1 + slp
 	system("ruby Tribal.rb -c farm")
-	break if $?.exitstatus != 0
-	f.close unless f.closed?
+	log.info("sleeping....")
+	if $?.exitstatus != 0
+		info.fatal("saida inexperada....")
+	end
 	sleep(slp)
 
 end
